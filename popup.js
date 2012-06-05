8ea0c40e8670564d8099085e7e20d47ac5b85b1b
Ulrich VACHON <u.vachon@gmail.com>
2012-06-05 22:23:15 +0200

    Ajout d un nouveau moteur de recherche et nouveau design

--- popup.js
+++ popup.js
@@ -4,6 +4,8 @@
 $(document).ready(function() {
    var NOM_TH_FICHIER = "Fichier";
 
+   var baseURL = chrome.extension.getURL('./');
+
    // cherche l'index de la colonne qui contient le fichier
    var indexThFichier = $("table#tabliste tr th:contains('" + NOM_TH_FICHIER + "')").index() + 1;
 
@@ -11,44 +13,42 @@ $(document).ready(function() {
    $("tr[class*='ligne'] td:nth-child(" + indexThFichier + ")").each(function() {
       var titre = $(this).html();
 
-      // ajout du lien par défaut
-      var liens = '<form style="display:inline;" action="http://binsearch.info/" method="get" target="_blank"><input type="hidden" name="q" value="' + titre + '"><a class="c16" id="binsearchLink" href="#">' + titre + '</a></form>';
-      liens += '&nbsp;';
-      liens += getBinsearchLink(titre);
-      liens += '&nbsp;';
-      liens += getYabsearchLink(titre);
+	  titre = titre.replace("&nbsp;", "");
 
-      $(this).html(liens);
+	  if (titre) {
+		  // ajout du lien par défaut
+		  var liens = '<form style="display:inline;" action="http://binsearch.info/" method="get" target="_blank"><input type="hidden" name="q" value="' + titre + '"><a onClick="$(this).parent().submit();" class="c16" href="#">' + titre + '</a></form>';
+		  liens += '&nbsp;';
+		  liens += '<img id="otherSites" width="12" height="12" src="' + baseURL + 'ampoule.png" data-nom="' + titre + '"/>';
+
+		  $(this).html(liens);
+      }
+   });
+
+   // ajoute le lien vers la popin des autres serveurs disponibles
+   $('img[id^=otherSites]').each(function() {
+	  var titre = $(this).data('nom');
+
+      $(this).CreateBubblePopup({
+		  innerHtml : getBinsearchLink('' + titre + '') + '&nbsp;' + getYabsearchLink('' + titre + '') + '&nbsp;' + getYubseLink('' + titre + ''),
+		  themePath : baseURL + 'jquerybubblepopup-themes',
+		  position : 'top',
+		  selectable : true
+	   });
    });
 
    // retourne le lien pour Binsearch
    function getBinsearchLink(titre) {
-      return '<form style="display:inline;" action="http://binsearch.info/" method="get" target="_blank"><input type="hidden" name="q" value="' + titre + '"><a class="c16" id="binsearchLink" href="#">' + getBinsearchImage() + '</a></form>';
+      return '<form style="display:inline;" action="http://binsearch.info/" method="get" target="_blank"><input type="hidden" name="q" value="' + titre + '"><a onClick="$(this).parent().submit();" class="c16" href="#"><img title="Binsearch.com" width="16" height="16" src="' + baseURL + 'binsearch.png"/></a></form>';
    }
 
    // retourne le lien pour Yabsearch
    function getYabsearchLink(titre) {
-      return '<form style="display:inline;" action="http://www.yabsearch.com/news/save" method="post" target="_blank"><input type="hidden" name="q" value="' + titre + '"><a class="c16" id="binsearchLink" href="#">' + getYabsearchImage() + '</a></form>';
+      return '<form style="display:inline;" action="http://www.yabsearch.com/news/save" method="post" target="_blank"><input type="hidden" name="q" value="' + titre + '"><a onClick="$(this).parent().submit();" class="c16" href="#"><img title="Yabsearch.com" width="16" height="16" src="' + baseURL + 'yabsearch.png"/></a></form>';
    }
 
-   // retourne l'image du logo pour Binsearch
-   function getBinsearchImage() {
-      return '<img title="Binsearch.com" width="8" height="8" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAACaSURBVDhPxVPbCcAgDOxoDuAYDuAAjuG/wziAAziAAzhAygkpPkBsbWlAiJC7eJd4CCFo5xw7YGAHghAC8VkhHwioiv8JnHOUcy5vgiyl1GD4VEItB3lK6R6BtZaklAXIYYxpSJZMhBQO5LW53xJorUs3GPhIAiYQY7zAuMOTJQno6r1vwPyiKUG/fZg9gH1nrnv/M63sf11zAkn/SrR2ETSdAAAAAElFTkSuQmCC"/>';
+   // retourne le lien pour Yubse
+   function getYubseLink(titre) {
+      return '<form style="display:inline;" action="http://www.yubse.com/?" method="get" target="_blank"><input type="hidden" name="q" value="' + titre + '"><a onClick="$(this).parent().submit();" class="c16" href="#"><img title="Yubse.com" width="16" height="16" src="' + baseURL + 'yubse.png"/></a></form>';
    }
-
-   // retourne l'image du logo pour Yabsearch
-   function getYabsearchImage() {
-      return '<img title="Yabsearch.com" width="8" height="8" src="data:image/png;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAUDBAQEAwUEBAQFBQUGBwwIBwcHBw8LCwkMEQ8SEhEPERETFhwXExQaFRERGCEYGh0dHx8fExciJCIeJBweHx7/2wBDAQUFBQcGBw4ICA4eFBEUHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh7/wAARCAAIAAgDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwD6X1248TJ450nSrYwR2F2ZJnugZGZUiaNjGVxtBYHaOeRuPUYJRRWk0lGNl0/VmVNycpXd9f0R/9k="/>';
-   }
-
-   // event branché sur la recherche sur Binnews
-   $("a[id=binsearchLink]").click(function() {
-       $(this).closest("form").submit();
-   });
-
-   // event branché sur la recherche sur Yabsearch
-   $("a[id=yabsearchLink]").click(function() {
-       $(this).closest("form").submit();
-   });
 });
-
