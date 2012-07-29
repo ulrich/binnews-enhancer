3d89b77af0f9a77b963814e95ccc5c61785ca3aa
Ulrich VACHON <u.vachon@gmail.com>
2012-07-29 21:59:56 +0200

    Ajout de la compatibilité avec la nouvelle version du site

--- popup.js
+++ popup.js
@@ -1,41 +1,43 @@
 // Copyright (c) 2012 Ulrich VACHON. All rights reserved.
 // Extension permettant d'ajouter un lien de téléchargement sur Binsearch.com
 
-$(document).ready(function() {
+$(document).ready(function () {
    var NOM_TH_FICHIER = "Fichier";
 
    var baseURL = chrome.extension.getURL('./');
 
    // cherche l'index de la colonne qui contient le fichier
-   var indexThFichier = $("table#tabliste tr th:contains('" + NOM_TH_FICHIER + "')").index() + 1;
+   var indexThFichier = $("table#table_results tr th:contains('" + NOM_TH_FICHIER + "')").index() + 1;
 
    // ajoute le lien dans le td de la colonne "Fichier"
-   $("tr[class*='ligne'] td:nth-child(" + indexThFichier + ")").each(function() {
+   $("tr td:nth-child(" + indexThFichier + ")").each(function () {
       var titre = $(this).html();
 
-	  titre = titre.replace("&nbsp;", "");
+      titre = titre.replace("&nbsp;", "");
 
-	  if (titre) {
-		  // ajout du lien par défaut
-		  var liens = '<form style="display:inline;" action="http://binsearch.info/" method="get" target="_blank"><input type="hidden" name="q" value="' + titre + '"><a onClick="$(this).parent().submit();" class="c16" href="#">' + titre + '</a></form>';
-		  liens += '&nbsp;';
-		  liens += '<img id="otherSites" width="12" height="12" src="' + baseURL + 'ampoule.png" data-nom="' + titre + '"/>';
-
-		  $(this).html(liens);
+      if (titre) {
+         // ajout du lien par défaut
+         var liens = '<div class="tech_temp" style="display:inline;"><input type="hidden" name="q" value="' + titre + '"><a onClick="$(this).parent().parent().submit();" class="c16" href="#">' + titre + '</a></div>';
+         liens += '&nbsp;';
+         liens += '<img id="otherSites" width="12" height="12" src="' + baseURL + 'ampoule.png" data-nom="' + titre + '"/>';
+         $(this).html(liens);
       }
    });
 
    // ajoute le lien vers la popin des autres serveurs disponibles
-   $('img[id^=otherSites]').each(function() {
-	  var titre = $(this).data('nom');
+   $('img[id^=otherSites]').each(function () {
+      var titre = $(this).data('nom');
 
       $(this).CreateBubblePopup({
-		  innerHtml : getBinsearchLink('' + titre + '') + '&nbsp;' + getYabsearchLink('' + titre + '') + '&nbsp;' + getYubseLink('' + titre + ''),
-		  themePath : baseURL + 'jquerybubblepopup-themes',
-		  position : 'top',
-		  selectable : true
-	   });
+         innerHtml:getBinsearchLink('' + titre + '') + '&nbsp;' + getYabsearchLink('' + titre + '') + '&nbsp;' + getYubseLink('' + titre + ''),
+         themePath:baseURL + 'jquerybubblepopup-themes',
+         position:'top',
+         selectable:true
+      });
    });
+
+   // post processing nécessaire pour rajouter à la volée les FORM des liens par défauts
+   $('.tech_temp').wrap('<form style="display:inline;" action="http://binsearch.info/" method="get" target="_blank"/>');
 
    // retourne le lien pour Binsearch
    function getBinsearchLink(titre) {
