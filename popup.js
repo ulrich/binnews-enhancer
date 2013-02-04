ed3f5df32522f4ae0e2bcede60e1b192f058f7d2
Ulrich VACHON <uvachon.prestataire@techno.voyages-sncf.com>
2013-02-04 13:45:16 +0100

    Removed href attribut to avoid loose page position

--- popup.js
+++ popup.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
 
       if (titre) {
          // ajout du lien par défaut
-         var liens = '<div class="tech_temp" style="display:inline;"><input type="hidden" name="q" value="' + titre + '"><a onClick="$(this).parent().parent().submit();" class="c16" href="#">' + titre + '</a></div>';
+         var liens = '<div class="tech_temp" style="display:inline;"><input type="hidden" name="q" value="' + titre + '"><a onClick="$(this).parent().parent().submit();" class="c16" >' + titre + '</a></div>';
          liens += '&nbsp;';
          liens += '<img id="otherSites" width="12" height="12" src="' + baseURL + 'ampoule.png" data-nom="' + titre + '"/>';
          $(this).html(liens);
@@ -47,32 +47,32 @@ $(document).ready(function () {
 
    // retourne le lien pour Binsearch
    function getBinsearchLink(titre) {
-      return '<form style="display:inline;" action="http://binsearch.info/" method="get" target="_blank"><input type="hidden" name="q" value="' + titre + '"><a onClick="$(this).parent().submit();" class="c16" href="#"><img title="Binsearch.com" width="16" height="16" src="' + baseURL + 'binsearch.png"/></a></form>';
+      return '<form style="display:inline;" action="http://binsearch.info/" method="get" target="_blank"><input type="hidden" name="q" value="' + titre + '"><a onClick="$(this).parent().submit();" class="c16" ><img title="Binsearch.com" width="16" height="16" src="' + baseURL + 'binsearch.png"/></a></form>';
    }
 
    // retourne le lien pour Yabsearch
    function getYabsearchLink(titre) {
-      return '<form style="display:inline;" action="http://www.yabsearch.com/news/save" method="post" target="_blank"><input type="hidden" name="q" value="' + titre + '"><a onClick="$(this).parent().submit();" class="c16" href="#"><img title="Yabsearch.com" width="16" height="16" src="' + baseURL + 'yabsearch.png"/></a></form>';
+      return '<form style="display:inline;" action="http://www.yabsearch.com/news/save" method="post" target="_blank"><input type="hidden" name="q" value="' + titre + '"><a onClick="$(this).parent().submit();" class="c16" ><img title="Yabsearch.com" width="16" height="16" src="' + baseURL + 'yabsearch.png"/></a></form>';
    }
 
    // retourne le lien pour Yubse
    function getYubseLink(titre) {
-      return '<form style="display:inline;" action="http://www.yubse.com/?" method="get" target="_blank"><input type="hidden" name="q" value="' + titre + '"><a onClick="$(this).parent().submit();" class="c16" href="#"><img title="Yubse.com" width="16" height="16" src="' + baseURL + 'yubse.png"/></a></form>';
+      return '<form style="display:inline;" action="http://www.yubse.com/?" method="get" target="_blank"><input type="hidden" name="q" value="' + titre + '"><a onClick="$(this).parent().submit();" class="c16" ><img title="Yubse.com" width="16" height="16" src="' + baseURL + 'yubse.png"/></a></form>';
    }
 
    // retourne le lien pour Misterbin
    function getMisterbinLink(titre) {
-      return '<form style="display:inline;" action="http://www.mysterbin.com/search?" method="get" target="_blank"><input type="hidden" name="q" value="' + titre + '"><a onClick="$(this).parent().submit();" class="c16" href="#"><img title="Misterbin.com" width="16" height="16" src="' + baseURL + 'mysterbin.png"/></a></form>';
+      return '<form style="display:inline;" action="http://www.mysterbin.com/search?" method="get" target="_blank"><input type="hidden" name="q" value="' + titre + '"><a onClick="$(this).parent().submit();" class="c16" ><img title="Misterbin.com" width="16" height="16" src="' + baseURL + 'mysterbin.png"/></a></form>';
    }
 
    // retourne le lien pour NZBIndex
    function getNZBIndexLink(titre) {
-      return '<form style="display:inline;" action="http://www.nzbindex.nl/search/?" method="get" target="_blank"><input type="hidden" name="q" value="' + titre + '"><a onClick="$(this).parent().submit();" class="c16" href="#"><img title="NZBIndex.com" width="16" height="16" src="' + baseURL + 'nzbindex.png"/></a></form>';
+      return '<form style="display:inline;" action="http://www.nzbindex.nl/search/?" method="get" target="_blank"><input type="hidden" name="q" value="' + titre + '"><a onClick="$(this).parent().submit();" class="c16" ><img title="NZBIndex.com" width="16" height="16" src="' + baseURL + 'nzbindex.png"/></a></form>';
    }
 
    // retourne le lien pour NZBClub
    function getNZBClubLink(titre) {
-      return '<form style="display:inline;" action="http://nzbclub.com/search.aspx?" method="get" target="_blank"><input type="hidden" name="q" value="' + titre + '"><a onClick="$(this).parent().submit();" class="c16" href="#"><img title="NZBClub.com" width="16" height="16" src="' + baseURL + 'nzbclub.png"/></a></form>';
+      return '<form style="display:inline;" action="http://nzbclub.com/search.aspx?" method="get" target="_blank"><input type="hidden" name="q" value="' + titre + '"><a onClick="$(this).parent().submit();" class="c16" ><img title="NZBClub.com" width="16" height="16" src="' + baseURL + 'nzbclub.png"/></a></form>';
    }
 
    // cherche l'index de la colonne qui contient le fichier avec un fallback causé pour la nouvelle version du site
